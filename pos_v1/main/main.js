203cdf72b8d8dd3032f8df9d3fecbb7ddfdcf440
CAROL T T LIANG (ITA-ISDC-ISD-OOCLL/ZHA) <CAROL T T LIANG (ITA-ISDC-ISD-OOCLL/ZHA) carol.t.t.liang@oocl.com>
2018-07-04 19:59:11 +0800

    update for posv1

--- pos_v1/main/main.js
+++ pos_v1/main/main.js
@@ -1,3 +1,82 @@
 'use strict';
 
-//TODO: 请在该文件中实现练习要求并删除此注释
+function printReceipt(tags){
+  let buyMap =  CalculateByItems(tags);
+  let buyItemWithDetail = CalBuyItemWithDetail(buyMap,loadAllItems())
+  let sum = buyItemSum(buyItemWithDetail);
+  let savePrice = saveItemSum(buyItemWithDetail,sum);
+  return print(buyItemWithDetail,sum,savePrice);
+}
+function CalculateByItems(tags){
+  //let buyItems = new Array();
+  let buyMap = {}
+  let buyItemSet = new Set()
+  for(let i = 0;i < tags.length;i++){
+    let key,value;
+    if(tags[i].indexOf('-')>-1){
+      key = tags[i].split('-')[0];
+      value = parseFloat(tags[i].split('-')[1]);
+    }else{
+      key = tags[i];
+      value = 1;
+    }
+    if(buyItemSet.has(key)){
+      buyMap[key] = buyMap[key]+value;
+    }else{
+      buyMap[key] = value;
+      buyItemSet.add(key);
+    }
+  }
+
+  // for(var key in buyMap) {
+  //   var item = {branch: key, count: buyMap[key]}
+  //   buyItems.push(item);
+  // }
+
+  return buyMap;
+}
+
+function CalBuyItemWithDetail(buyMap,allItems){
+  let buyItemWithDetail = new Array();
+     for(let i= 0;i<allItems.length;i++){
+       let item = allItems[i];
+       if(buyMap.hasOwnProperty(item.barcode)){
+         let buyItem = {};
+         buyItem['name'] = item.name;
+         let count = buyMap[item.barcode];
+         buyItem['count'] = count;
+         buyItem['price'] = item.price;
+         buyItem['summary'] = item.price*(parseInt(count/3)*2+(count%3));
+         buyItem['unit'] = item.unit;
+         buyItemWithDetail.push(buyItem);
+       }
+     }
+     return buyItemWithDetail;
+}
+
+function buyItemSum(buyItemWithDetail){
+  let sum = 0;
+  for(let i = 0;i<buyItemWithDetail.length;i++){
+    sum += buyItemWithDetail[i].summary;
+  }
+  return sum;
+}
+function saveItemSum(buyItemWithDetail,sum){
+  let savePrice = parseInt(0);
+  for(let i = 0;i<buyItemWithDetail.length;i++){
+    savePrice += buyItemWithDetail[i].count*buyItemWithDetail[i].price;
+  }
+  savePrice  = savePrice - sum;
+  return savePrice;
+}
+
+function print(buyItemWithDetail,sum,savePrice){
+  let receipt = '***<没钱赚商店>收据***\n'
+  for(let i= 0;i<buyItemWithDetail.length;i++) {
+    let item = buyItemWithDetail[i];
+    receipt += '名称：' + item.name + '，数量：' + item.count + item.unit + '，单价：' + item.price.toFixed(2) + '(元)，小计：' + item.summary.toFixed(2) + '(元)\n'
+  }
+  receipt+='----------------------\n总计：'+sum.toFixed(2)+'(元)\n节省：'+savePrice.toFixed(2)+'(元)\n**********************';
+    return receipt;
+  }
+
--- pos_v1/test/main-test.js
+++ pos_v1/test/main-test.js
@@ -17,7 +17,7 @@ describe('pos', () => {
 
     spyOn(console, 'log');
 
-    printReceipt(tags);
+    let receipt = printReceipt(tags);
 
     const expectText = `***<没钱赚商店>收据***
 名称：雪碧，数量：5瓶，单价：3.00(元)，小计：12.00(元)
@@ -27,7 +27,7 @@ describe('pos', () => {
 总计：58.50(元)
 节省：7.50(元)
 **********************`;
-
+    console.log(receipt)
     expect(console.log).toHaveBeenCalledWith(expectText);
   });
 });
